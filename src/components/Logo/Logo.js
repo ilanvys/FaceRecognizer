2ac48e131a6e442259e5b076b14397ef96202270
ilanvys <ilanvys@gmail.com>
2023-10-25 01:26:58 +0300

    Add Basic Project Components

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import brain from './brain-logo.png'
+import './Logo.css'
+
+
+const Logo = () => {
+  return (
+    <div className='h4 w4 ma4 mt0'>
+      <Tilt className='wrapper' >
+        <div className='wrapper br2 shadow-2 pa3' >
+          <img alt='Face Recognizer' src={brain} />
+        </div>
+      </Tilt>
+    </div>
+  )
+}
+
+export default Logo;
